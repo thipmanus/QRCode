ffd0fca337983834b87ce92514bfa7721f00e9f8
thipmanus <5910110148@psu.ac.th>
2019-07-27 23:35:33 +0700

    fix login

--- src/Loginscreen.js
+++ src/Loginscreen.js
@@ -65,7 +65,7 @@ function getDate(select){
 
 function encrypt(word){
   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-.:;=?@[]^_`{|}~";
-  var permuted = "QU2HAd0w3VCspWrokXmyNzYbIJKvLMhij85DOn1Sce49gP7BEuTfZ6FxqRlatG!#$%&()*+,-.:;=?@[]^_`{|}~";
+
  //input
   var i=0;
   var result = "";　
@@ -78,44 +78,8 @@ function encrypt(word){
   }
   return result;
 }
-
-function checklogin(){
-  var id = 'RG6Z66ZRax'
-  var password = '28KTKScS_ZF'
-  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('POST', 'http://opac.psu.ac.th/PatronService.asmx?op=GetPatron', true );
-    var sr = 
-            '<?xml version="1.0" encoding="utf-8"?>' +
-            '<soap:Envelope ' +
-                'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-                'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' + 
-                'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> ' +
-                '<soap:Body> ' +
-                    '<GetPatron xmlns="http://opac.psu.ac.th/">' +
-                        '<username>'+id+'</username> ' +
-                        '<password>'+password+'</password> ' +
-                    '</GetPatron> ' +
-                '</soap:Body> ' +
-            '</soap:Envelope>' ;
-    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-    xmlhttp.send(sr);
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-          var resultxml = xmlhttp.responseText;
-          var i = 0
-          while(i < resultxml.length){
-            if(resultxml.substring(i,i+17) === "<GetPatronResult>"){
-                var resultFromXml = resultxml.substr(i+17,1)
-                console.log('จาก xml ' + resultFromXml)
-            }
-            i++
-          }
-          //return resultFromXml;
-        }
-    };
-}
-
+var testId;
+var testPassword;
 class Loginscreen extends Component {
   constructor(props){
     super(props);
@@ -134,7 +98,49 @@ class Loginscreen extends Component {
       passEncrypt:'',
     }
   }
+  sendRequest() {
+    var resultFromXml
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open('POST', 'http://opac.psu.ac.th/PatronService.asmx?op=GetPatron', true );
+      var sr = 
+              '<?xml version="1.0" encoding="utf-8"?>' +
+              '<soap:Envelope ' +
+                  'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+                  'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' + 
+                  'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> ' +
+                  '<soap:Body> ' +
+                      '<GetPatron xmlns="http://opac.psu.ac.th/">' +
+                          '<username>'+testId+'</username> ' +
+                          '<password>'+testPassword+'</password> ' +
+                      '</GetPatron> ' +
+                  '</soap:Body> ' +
+              '</soap:Envelope>' ;
+      xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+      xmlhttp.send(sr);
+      xmlhttp.onreadystatechange = function (e) {
+          if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var resultxml = xmlhttp.responseText;
+            var i = 0
+            while(i < resultxml.length){
+              if(resultxml.substring(i,i+17) === "<GetPatronResult>"){
+                  resultFromXml = resultxml.substr(i+17,1)
+                  this.setState({setResult:resultFromXml});
+                  console.log('จาก xml ' + resultFromXml)
+                  console.log('จาก id ' + testId)
+                  console.log('จาก password ' + testPassword)
+              }
+              i++
+            }
+            //valueCallBack(resultFromXml);
+          }
+      }.bind(this);
+    }
   handleClick(){
+    testId = encrypt(this.state.username)
+    testPassword = encrypt(this.state.password)
+    this.sendRequest()
+    //testRequest(testId,testPassword)
     this.setState({userEncrypt:encrypt(this.state.username)})
     this.setState({passEncrypt:encrypt(this.state.password)})
     //this.setState({UserNumber:this.state.UserNumber + 1})
@@ -142,28 +148,21 @@ class Loginscreen extends Component {
     this.setState({key:getRndInteger()})
     this.setState({dateTime:getDate('stamp')})
     this.setState({QRID:'TM' + this.state.username + /* pad(this.state.UserNumber + 1, 3)+ */ this.state.key + getDate('gen'), size:250,})
-    this.setState({setResult:checklogin()})
+    //this.setState({setResult:'1'})
   }
   render() {
     const style = {
       margin:30,
     }
+    //console.log('ใน render ' + val)
     if(this.state.setResult === '1' ){
       return(
         <div>
           <MuiThemeProvider key = {"theme"}>
-          <AppBar title="QrCode Page"/>
+          <AppBar title="QrCode"/>
         </MuiThemeProvider>
         <MuiThemeProvider > 
         <div>
-        <br/>
-         {'ID : ' + this.state.userEncrypt}
-        <br/>
-        <br/>
-         {'Pass : ' + this.state.passEncrypt}
-        <br/>
-        <br/>
-         {'Key : ' + this.state.genKey}
         <br/>
         <br/>
         {'QRCODE ID : ' + this.state.QRID}
@@ -183,17 +182,52 @@ class Loginscreen extends Component {
        </div>
       );
     }
+    else if(this.state.setResult === '0' ){
+      return (
+        <div>
+          <MuiThemeProvider key = {"theme"}>
+            <AppBar title="Sign in PSU Passport"/>
+          </MuiThemeProvider>
+            <MuiThemeProvider > 
+          <div>
+           <TextField
+             hintText="Enter student ID"
+             floatingLabelText="Student ID"
+             onChange={(event,newValue) => this.setState({username:newValue})}
+             />
+             <br/>
+             <TextField
+               type="password"
+               hintText="Enter Password"
+               floatingLabelText="Password"
+               onChange={(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             
+             <RaisedButton 
+              label="Generate QR Code" 
+              primary={true}
+              style = {style}
+              onClick={() => this.handleClick()}/>
+  
+              <br/>
+             {'id หรือ password ไม่ถูกต้อง : ' + this.state.setResult}
+         </div>   
+          </MuiThemeProvider>
+         </div>
+      );
+    }
     return (
       <div>
         <MuiThemeProvider key = {"theme"}>
-          <AppBar title="Login Page Exeriment"/>
+          <AppBar title="Sign in PSU Passport"/>
         </MuiThemeProvider>
           <MuiThemeProvider > 
         <div>
          <TextField
            hintText="Enter student ID"
            floatingLabelText="Student ID"
-           onChange={(event,newValue,text) => this.setState({username:newValue , inputValue: text})}
+           onChange={(event,newValue) => this.setState({username:newValue})}
            />
            <br/>
            <TextField
@@ -209,9 +243,7 @@ class Loginscreen extends Component {
             primary={true}
             style = {style}
             onClick={() => this.handleClick()}/>
-
             <br/>
-           {'state : ' + this.state.setResult}
        </div>   
         </MuiThemeProvider>
        </div>
