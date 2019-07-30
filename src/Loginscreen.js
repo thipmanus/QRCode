5cbabd55c2a128b52a774d747c39f743c22ccb2f
thipmanus <5910110148@psu.ac.th>
2019-07-30 09:32:24 +0700

    fix sign in secur

--- src/Loginscreen.js
+++ src/Loginscreen.js
@@ -4,6 +4,7 @@ import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import QRCode from 'qrcode.react'
+/*
 function pad(number, length) {
    
     var str = '' + number;
@@ -14,62 +15,73 @@ function pad(number, length) {
     return str;
 
 }
-function getRndInteger() {
+function firstgetRndInteger() {
   var keyNumber = '';
-  keyNumber = Math.floor(Math.random() * (999) ) + 1;
+  keyNumber = Math.floor(Math.random() * (500) ) + 1;
   keyNumber = pad(keyNumber, 3)
   return keyNumber;
 }
+function lastgetRndInteger() {
+  var keyNumber = '';
+  keyNumber = Math.floor(Math.random() * (499) ) + 501;
+  keyNumber = pad(keyNumber, 3)
+  return keyNumber;
+}
+*/
 function getDate(select){
-  var dateNow = '';
+  var dateNext = '';
   var dateTime = '';
+  var dateExpire = '';
   var today = new Date();
   var timetoday = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; 
-  var yyyy = today.getFullYear();
-  var HH = today.getHours();
-  var MM = today.getMinutes();
-  var SS = today.getSeconds();
-  if(dd<10) 
-  {
-      dd='0'+dd;
-  } 
-  if(mm<10) 
-  {
-      mm='0'+mm;
-  }
-  if(HH<10) 
-  {
-      HH='0'+HH;
-  } 
-  if(MM<10) 
-  {
-      MM='0'+MM;
-  }
-  if(SS<10) 
-  {
-      SS='0'+SS;
-  }  
-  today = dd+mm+HH+MM+SS;
-  dateNow = today;
-  timetoday = dd+'/'+mm+'/'+yyyy+' '+HH+':'+MM+':'+SS;
+  var expiretoday = new Date();
+  var days = today.getDate();
+  var year = today.getFullYear();
+  var month = (today.getMonth()+1);
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  var nexttime =  new Date(today.getTime() + 5*60000);
+  var nextdays = nexttime.getDate();
+  var nextyear = nexttime.getFullYear();
+  var nextmonth = (nexttime.getMonth()+1);
+  var nexthours = nexttime.getHours();
+  var nextminutes = nexttime.getMinutes();
+  var nextseconds = nexttime.getSeconds();
+  days = days < 10 ? '0' + days : days;
+  month = month < 10 ? '0' + month : month;
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  nextdays = nextdays < 10 ? '0' + nextdays : nextdays;
+  nextmonth = nextmonth < 10 ? '0' + nextmonth : nextmonth;
+  nexthours = nexthours < 10 ? '0' + nexthours : nexthours;
+  nextminutes = nextminutes < 10 ? '0' + nextminutes : nextminutes;
+  nextseconds = nextseconds < 10 ? '0' + nextseconds : nextseconds;  
+  today = days + month + year + hours + minutes + seconds;
+  nexttime = nextdays + nextmonth + nextyear + nexthours + nextminutes + nextseconds;
+  dateNext = nexttime;
+  timetoday = days + '/' + month + '/' + year + ' ' + hours + ':' + minutes + ':' + seconds;
   dateTime = 'เวลาเข้าใช้งาน : ' + timetoday
+  expiretoday = nextdays + '/' + nextmonth + '/' + nextyear + ' ' + nexthours + ':' + nextminutes + ':' + nextseconds;
+  dateExpire = 'Qrcode สามารถใช้งานได้ถึง : ' + expiretoday
   if(select === 'stamp'){
     return dateTime;
   }
   else if (select === 'gen'){
-    return dateNow;
+    return dateNext;
+  }
+  else if (select === 'expire'){
+    return dateExpire;
   }
 }
-
 function encrypt(word){
   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-.:;=?@[]^_`{|}~";
-
+  
  //input
   var i=0;
   var result = "";　
-  //loop receive 
+  //loop receive
   while (i < word.length) {
       //ถ้า word ไม่อยู่ใน alphabet จะออกมาเป็นตัวเดิม
   　　var ind = alphabet.indexOf(word.charAt(i));
@@ -88,11 +100,14 @@ class Loginscreen extends Component {
       username:'',
       password:'',
       QRID:'',
+      previousID:'',
       size:0,
-      //UserNumber:0,
-      key:getRndInteger(),
+      //firstkey:firstgetRndInteger(),
+      //lastkey:lastgetRndInteger(),
       dateTime:'',
-      genKey:'',
+      expireTime:'',
+      firstgenKey:'',
+      lastgenKey:'',
       setResult:'start',
       userEncrypt:'',
       passEncrypt:'',
@@ -127,12 +142,9 @@ class Loginscreen extends Component {
                   resultFromXml = resultxml.substr(i+17,1)
                   this.setState({setResult:resultFromXml});
                   console.log('จาก xml ' + resultFromXml)
-                  console.log('จาก id ' + testId)
-                  console.log('จาก password ' + testPassword)
               }
               i++
             }
-            //valueCallBack(resultFromXml);
           }
       }.bind(this);
     }
@@ -140,15 +152,31 @@ class Loginscreen extends Component {
     testId = encrypt(this.state.username)
     testPassword = encrypt(this.state.password)
     this.sendRequest()
-    //testRequest(testId,testPassword)
     this.setState({userEncrypt:encrypt(this.state.username)})
     this.setState({passEncrypt:encrypt(this.state.password)})
-    //this.setState({UserNumber:this.state.UserNumber + 1})
-    this.setState({genKey:this.state.key})
-    this.setState({key:getRndInteger()})
+    this.setState({firstgenKey:this.state.firstkey})
+    this.setState({lastgenKey:this.state.lastkey})
+    //this.setState({firstkey:firstgetRndInteger()})
+    //this.setState({lastkey:firstgetRndInteger()})
     this.setState({dateTime:getDate('stamp')})
-    this.setState({QRID:'TM' + this.state.username + /* pad(this.state.UserNumber + 1, 3)+ */ this.state.key + getDate('gen'), size:250,})
-    //this.setState({setResult:'1'})
+    this.setState({expireTime:getDate('expire')})
+    this.setState({previousID:'TM'+ getDate('gen') + this.state.username})
+    this.setState({QRID:'TM'+ encrypt(getDate('gen') + this.state.username), size:250,})
+  }
+  signOutWeb(){
+    this.setState({username:''})
+    this.setState({password:''})
+    this.setState({userEncrypt:''})
+    this.setState({passEncrypt:''})
+    this.setState({firstgenKey:''})
+    this.setState({lastgenKey:''})
+    //this.setState({firstkey:firstgetRndInteger()})
+    //this.setState({lastkey:lastgetRndInteger()})
+    this.setState({dateTime:''})
+    this.setState({expireTime:''})
+    this.setState({previousID:''})
+    this.setState({QRID:'', size:0,})
+    this.setState({setResult:'start'})
   }
   render() {
     const style = {
@@ -159,13 +187,13 @@ class Loginscreen extends Component {
       return(
         <div>
           <MuiThemeProvider key = {"theme"}>
-          <AppBar title="QrCode"/>
+          <AppBar title="QrCode" showMenuIconButton={false}/>
         </MuiThemeProvider>
         <MuiThemeProvider > 
         <div>
         <br/>
         <br/>
-        {'QRCODE ID : ' + this.state.QRID}
+        {'ขอบคุณที่ใช้บริการค่ะ'}
          <br/>
         <br/>
            <QRCode
@@ -176,9 +204,34 @@ class Loginscreen extends Component {
            />
            <br/>
            <br/>
-            {this.state.dateTime}
+            {this.state.expireTime}
+            <br/>
+            <br/>
+            หากไม่สามารถเข้าใช้งานห้องสมุดได้ทันในเวลาที่กำหนด
+            <br/>
+            สามารถลงชื่อเข้าใช้ใหม่อีกครั้งเพื่อสร้าง QrCode ได้ค่ะ
+           <br/>
+            <RaisedButton 
+              label="Sign Out" 
+              primary={true}
+              style = {style}
+              onClick={() => this.signOutWeb()}/>
        </div> 
        </MuiThemeProvider>
+       <br/>
+       {'username : ' + this.state.username}
+       <br/>
+       {'userEncrypt : ' + this.state.userEncrypt}
+       <br/>
+       {'setResult : ' + this.state.setResult}
+       <br/>
+       {'Previous ID : ' + this.state.previousID}
+       <br/>
+       {'QRCODE ID : ' + this.state.QRID}
+       <br/>
+       {'dateTime : ' + this.state.dateTime}
+       <br/>
+       {'expireTime : ' + this.state.expireTime}
        </div>
       );
     }
@@ -186,10 +239,13 @@ class Loginscreen extends Component {
       return (
         <div>
           <MuiThemeProvider key = {"theme"}>
-            <AppBar title="Sign in PSU Passport"/>
+            <AppBar title="Sign in PSU Passport" showMenuIconButton={false}/>
           </MuiThemeProvider>
             <MuiThemeProvider > 
           <div>
+          <br/>
+             {'id หรือ password ไม่ถูกต้อง '}
+             <br/>
            <TextField
              hintText="Enter student ID"
              floatingLabelText="Student ID"
@@ -205,22 +261,34 @@ class Loginscreen extends Component {
              <br/>
              
              <RaisedButton 
-              label="Generate QR Code" 
+              label="Sign In" 
               primary={true}
               style = {style}
               onClick={() => this.handleClick()}/>
   
-              <br/>
-             {'id หรือ password ไม่ถูกต้อง : ' + this.state.setResult}
          </div>   
           </MuiThemeProvider>
+          <br/>
+       {'username : ' + this.state.username}
+       <br/>
+       {'userEncrypt : ' + this.state.userEncrypt}
+       <br/>
+       {'setResult : ' + this.state.setResult}
+       <br/>
+       {'Previous ID : ' + this.state.previousID}
+       <br/>
+       {'QRCODE ID : ' + this.state.QRID}
+       <br/>
+       {'dateTime : ' + this.state.dateTime}
+       <br/>
+       {'expireTime : ' + this.state.expireTime}
          </div>
       );
     }
     return (
       <div>
         <MuiThemeProvider key = {"theme"}>
-          <AppBar title="Sign in PSU Passport"/>
+          <AppBar title="Sign in PSU Passport" showMenuIconButton={false}/>
         </MuiThemeProvider>
           <MuiThemeProvider > 
         <div>
@@ -239,13 +307,27 @@ class Loginscreen extends Component {
            <br/>
            
            <RaisedButton 
-            label="Generate QR Code" 
+            label="Sign In" 
             primary={true}
             style = {style}
             onClick={() => this.handleClick()}/>
             <br/>
        </div>   
         </MuiThemeProvider>
+        <br/>
+       {'username : ' + this.state.username}
+       <br/>
+       {'userEncrypt : ' + this.state.userEncrypt}
+       <br/>
+       {'setResult : ' + this.state.setResult}
+       <br/>
+       {'Previous ID : ' + this.state.previousID}
+       <br/>
+       {'QRCODE ID : ' + this.state.QRID}
+       <br/>
+       {'dateTime : ' + this.state.dateTime}
+       <br/>
+       {'expireTime : ' + this.state.expireTime}
        </div>
     );
   }
